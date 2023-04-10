1ae16bd3dc8fb0a88df1da5bd7d161729334507d
SashoAlex2000 <sahooo3@abv.bg>
2023-04-10 21:21:59 +0300

    feat: restyled details page

--- src/views/details.js
+++ src/views/details.js
@@ -2,60 +2,81 @@
 import { getSpecificComments, postComment } from "../data/comments.js";
 import { getMovieById } from "../data/movie.js";
 import { html } from "../lib/lit-html.js";
-import { createRatingObject, createSubmiteHandler } from "../util.js";
+import { createRatingObject, createSubmiteHandler, movieCategories, movieOptions } from "../util.js";
 import { repeat } from '../lib/directives/repeat.js'
 import { classMap } from '../lib/directives/class-map.js'
 import { deleteRating, getMovieRatingsById, postRating } from "../data/movieRating.js";
 
-const detailTemplate = (onComment, onRate, movie, currentComments, ratingObject) => html`
-    <h1>${movie.name}</h1>
-    <h3>${movie.year}</h3>
-    <h3>${movie.category}</h3>
-    <h3>${movie.rating}</h3>
-    <p>${movie.description}</p>
-    <p class="rating holder" id="${ratingObject.ratingId}">
-        Movie Rating: ${!isNaN(ratingObject.average) ? ratingObject.average: "-"} / 5 (${ratingObject.length})
-    </p>
-    <br>
+const detailTemplate = (
+    onComment, onRate, movie, currentComments, ratingObject, movieOptions, movieCategories
+    ) => html`
+    <div class="details-wrapper">
 
-    <form @submit=${onRate}>
-        <label for="rating">Rate this movie</label>
+        <div class="details-content">
+            <div class="details-text">
+                <h1>${movie.name}</h1>
+                <h3>${movie.year}</h3>
+                <h3>${movieCategories[movie.category]}</h3>
+                <h3>${movieOptions[movie.rating]}</h3>
+                <p>${movie.description}</p>
+                <p class="rating holder" id="${ratingObject.ratingId}">
+                    Movie Rating: ${!isNaN(ratingObject.average) ? ratingObject.average: "-"} / 5 (${ratingObject.length})
+                </p>
+                <form @submit=${onRate}>
+                <label for="rating">Rate this movie</label>
+    
+                ${ratingObject.hasRated ? html `
+                <p>Your Rating: ${ratingObject.currentUserRating}</p>
+                ` : html `
+                <select id="rating" name="rating">
+                    <option calue="1">1</option>
+                    <option calue="2">2</option>
+                    <option calue="3">3</option>
+                    <option calue="4">4</option>
+                    <option calue="5">5</option>
+                </select>
+                `}
+                
+                ${ratingObject.hasRated ? html `
+                <button>Unrate</button>
+                ` : html `
+                <button>Rate</button>
+                `}
+                
+            </form> 
+            </div>
+            <div class="details-image">
+                ${movie.imageUrl ? html `
+                <img src=${movie.imageUrl} class="catalog-img">
+                `: html `
+                <img src="../../static/images/default.jpeg" class="catalog-img">
+                `}
+            </div>
+            <br>
+        </div>
 
-        ${ratingObject.hasRated ? html `
-        <p>Your Rating: ${ratingObject.currentUserRating}</p>
-        ` : html `
-        <select id="rating" name="rating">
-            <option calue="1">1</option>
-            <option calue="2">2</option>
-            <option calue="3">3</option>
-            <option calue="4">4</option>
-            <option calue="5">5</option>
-        </select>
-        `}
+
         
-        ${ratingObject.hasRated ? html `
-        <button>Unrate</button>
-        ` : html `
-        <button>Rate</button>
-        `}
-        
-    </form>
-    <br>
+        <div class="comments">
+            <form @submit="${onComment}" class="comment-submit-form">
+                <input name="commentText" placeholder="Enter your comment"></input>
+                <button type="submit" class="comment-submit-button">Comment</button>
+                <button type="button" class="comment-submit-button cancel">Cancel</button>
+            </form>
 
-    <form @submit="${onComment}">
-        <input name="commentText" placeholder="Enter your comment"></input>
-        <button>Comment!</button>
-    </form>
+            <div class="comment-line-div">
+            </div>
 
-    ${currentComments.length > 0 ?
-        html`
-        <ul>
-            ${repeat(currentComments, c => c.objectId, commentCard)}
-        </ul>
-        `
-        :
-        html`No comments yet!`}
-
+            ${currentComments.length > 0 ?
+                html`
+                <ul>
+                    ${repeat(currentComments, c => c.objectId, commentCard)}
+                </ul>
+                `
+                :
+                html`<p>Be the first one to comment!</p>`}
+        </div>  
+    </div>    
 `;
 
 const commentCard = (comment) => html`
@@ -82,7 +103,43 @@ export async function showDetails(ctx) {
     const ratingObject = createRatingObject(allRatings, ctx.user?.objectId);
 
     ctx.render(detailTemplate(createSubmiteHandler(onCommentCreate, true), createSubmiteHandler(onRate),
-    movie, currentComments, ratingObject));
+    movie, currentComments, ratingObject, movieOptions, movieCategories));
+
+    const commentInput = document.getElementsByTagName('input')[0];
+    commentInput.addEventListener('focusin', revealButtons);
+    commentInput.addEventListener('input', onCommentInput)
+    const buttons = document.getElementsByClassName('comment-submit-button');
+    const submitButton = buttons[0];
+    const cancelButton = buttons[1];
+    cancelButton.addEventListener('click', hideButtons);
+
+    function revealButtons (event) {
+        for (let button of buttons) {
+            button.style.display = 'inline-block'
+        }
+    }
+
+    function hideButtons (event) {
+        for (let button of buttons) {
+            button.style.display = 'none';
+        }
+        commentInput.value = '';
+        if (submitButton.classList.contains('blue-btn')) {
+            submitButton.classList.remove('blue-btn');
+        } 
+    }
+
+    function onCommentInput (event) {
+        if (event.target.value == '') {
+            if (submitButton.classList.contains('blue-btn')) {
+                submitButton.classList.remove('blue-btn');
+            }
+        } else {
+            if (!submitButton.classList.contains('blue-btn')) {
+                submitButton.classList.add('blue-btn');
+            }
+        }
+    }
 
     async function onCommentCreate({ commentText }) {
 
